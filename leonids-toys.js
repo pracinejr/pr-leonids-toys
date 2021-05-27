72ce9c3ed3ade01642f3ae36c1358d77aa91a906
Peter Racine Jr <peterracinejr@gmail.com>
2021-05-27 13:42:17 -0500

    made the first array in main on accident

--- leonids-toys.js
+++ leonids-toys.js
@@ -1,27 +1,29 @@
-const Leonardo = {
-  id: 1,
-  name: "Leonardo",
-  type: "Ninja Turtle",
-  color: "Emerald Green",
-  manufacturer: "Playmates Holdings Ltd",
-  price: 22.99
-}
+const toys = [
+  {
+    id: 1,
+    name: "Leonardo",
+    type: "Ninja Turtle",
+    color: "Emerald Green",
+    manufacturer: "Playmates Holdings Ltd",
+    price: 22.99,
+  },
 
-const IronMan = {
-  id: 2,
-  name: "Iron Man",
-  type: "Action Figure",
-  color: "red and gold",
-  manufacturer: "Hasboro Pluse",
-  price: 20.00
-}
+  {
+    id: 2,
+    name: "Iron Man",
+    type: "Action Figure",
+    color: "red and gold",
+    manufacturer: "Hasboro Pluse",
+    price: 20.0,
+  },
 
-const Cyberpubk = {
-  id: 3,
-  name: "Cyberpunk",
-  type: "Action Figure",
-  color: "black and brown",
-  manufacturer: "McFarlane Toys",
-  price: 26.95
-}
-
+  {
+    id: 3,
+    name: "Cyberpunk",
+    type: "Action Figure",
+    color: "black and brown",
+    manufacturer: "McFarlane Toys",
+    price: 26.95,
+  },
+];
+console.log(toys);
